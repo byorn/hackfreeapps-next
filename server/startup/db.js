bd38d3a5cb71a3364bf3f35e56a313704886235d
hackfreeapps <52660141+hackfreeapps@users.noreply.github.com>
2020-12-23 21:38:30 +1100

    changed db setting

--- server/startup/db.js
+++ server/startup/db.js
@@ -4,9 +4,9 @@ const dbInit = async function(){
     mongoose.Promise = global.Promise;
 
     const uri = "mongodb+srv://hackfreeapps:hackfreeapps@cluster0.fwlx7.mongodb.net/hackfreeapps?retryWrites=true&w=majority";
-    const uri1 = "mongodb://localhost:27017/hackfreeapps";
+    //const uri1 = "mongodb://localhost:27017/hackfreeapps";
 
-    await mongoose.connect(uri1,{useNewUrlParser: true,useUnifiedTopology: true });
+    await mongoose.connect(uri,{useNewUrlParser: true,useUnifiedTopology: true });
 };
 
 
